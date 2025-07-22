834c308b8f798e7b4a73895188dbb34690b8db3d
Ahammed Anas <anassabu123@gmail.com>
2025-07-22 18:46:09 +0400

    Fix: Updated views and urls for home and aboutus pages

--- myproject/urls.py
+++ myproject/urls.py
@@ -6,23 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.Home,name='home'),
-    path('aboutus/', views.Aboutus,name='aboutus'),
-    path('contactus/', views.Contactus,name='contactus'),
-    path('booknow/<str:docId>/',views.Booknow,name='booknow'),
-    path('addYatch/',views.addYacht,name='addyacht'),
-    path('saveYachtInfo/',views.saveYachtInfo,name='saveYachtInfo'),
-    path('addEvent/',views.addEvent,name='addEvent'),
-    path('saveEventInfo/',views.saveEventInfo,name='saveEventInfo'),
-    path('callRequest/',views.callRequest,name='callRequest'),
-    path('bookingRequest/',views.bookingRequest,name='bookingRequest')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-from django.contrib import admin
-from django.urls import path
-from web import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-]
+    path('aboutus/', views.aboutus, name='aboutus'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- web/views.py
+++ web/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Welcome to Sandiyago Yachts!")
+
+def aboutus(request):
+    return HttpResponse("About Sandiyago Yachts - Luxury, Adventure & Memories.")
